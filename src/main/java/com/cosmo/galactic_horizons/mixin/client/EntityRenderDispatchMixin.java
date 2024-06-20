346269a3eabb929d025804f34abfde3d878707c6
cosmo <curtisjibo@gmail.com>
2024-06-20 16:05:59 -0500

    t

--- src/main/java/com/cosmo/galactic_horizons/mixin/client/EntityRenderDispatchMixin.java
+++ src/main/java/com/cosmo/galactic_horizons/mixin/client/EntityRenderDispatchMixin.java
@@ -9,6 +9,7 @@ import net.minecraft.client.render.entity.EntityRenderDispatcher;
 import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.world.WorldView;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -17,9 +18,12 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(EntityRenderDispatcher.class)
 public class EntityRenderDispatchMixin {
-	@Inject(method = "renderShadow", at = @At("HEAD"),cancellable = true)
-	private static void renderShadow(MatrixStack matrices, VertexConsumerProvider vertexConsumers, Entity entity, float opacity, float tickDelta, WorldView world, float radius, CallbackInfo ci){
-		if (GalacticHorizonsClient.entities.contains(entity.getUuid())) ci.cancel();
+	@Inject(method = "renderShadow",at=@At("HEAD"),cancellable = true)
+	private static void renderShadow(MatrixStack matrices, VertexConsumer vertices, Entity entity, float tickDelta, CallbackInfo ci){
+		if(entity instanceof LivingEntity) {
+			if (((LivingEntity) entity).getStatusEffects() != null && ((LivingEntity) entity).hasStatusEffect(ModEffects.REALITY_TEAR) || ((LivingEntity) entity).hasStatusEffect(ModEffects.REALITY_SPLIT))
+				ci.cancel();
+		}
 	}
 	@Inject(method = "renderHitbox", at = @At("HEAD"),cancellable = true)
 	private static void renderHitbox(MatrixStack matrices, VertexConsumer vertices, Entity entity, float tickDelta, CallbackInfo ci) {
@@ -30,6 +34,9 @@ public class EntityRenderDispatchMixin {
 	}
 	@Inject(method = "renderFire",at = @At("HEAD"),cancellable = true)
 	private void renderFire(MatrixStack matrices, VertexConsumerProvider vertexConsumers, Entity entity, CallbackInfo ci) {
-		if(GalacticHorizonsClient.entities.contains(entity.getUuid())) ci.cancel();
+		if(entity instanceof LivingEntity) {
+			if (((LivingEntity) entity).getStatusEffects() != null && ((LivingEntity) entity).hasStatusEffect(ModEffects.REALITY_TEAR) || ((LivingEntity) entity).hasStatusEffect(ModEffects.REALITY_SPLIT))
+				ci.cancel();
+		}
 	}
 }
